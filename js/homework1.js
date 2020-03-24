23dfac1c92f14751447adc5689072bbfbaedffaa
foma1337 <nifominov@yandex.ru>
2020-03-24 18:45:13 +0300

    homework 3,4

--- js/homework1.js
+++ js/homework1.js
@@ -21,3 +21,4 @@ if (purchase >= 3000){
     console.log("skidka 10%+podarok");
 }
 
+
--- js/homework3.js
+++ js/homework3.js
@@ -6,7 +6,23 @@
     3. пользователь ввел число больше загадонного - вывод в консоль "«агаданное число меньше", просьба угадать число повтор€етс€
     4. пользователь ввел число меньше загадонного - вывод в консоль "«агаданное число больше", просьба угадать число повтор€етс€*/
 
-let 
-while {
-    focus = prompt("ygaday chislo");
+let answer = Math.floor(Math.random()*7 + 1);
+for (let i = 1; answer; i = 1) {
+	let user_data = prompt("vvedite chislo");
+	if (parseInt(user_data) === 0) {
+		console.log("Quit");
+		break;
+	}
+	if (parseInt(user_data) === 5) {
+		console.log("Vi ygadali");
+		break;
+	}
+	if (parseInt(user_data) > answer) {
+		console.log("zagadannoe chislo menshe");
+		continue;
+	}
+	if (parseInt(user_data) < answer) {
+		console.log("zagadannoe chislo bolshe");
+		continue;
+	}
 }
